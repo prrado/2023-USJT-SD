65fcaf41501779b6ff4315a20804a232cba14fbd
Caio Ryan <74019374+prrado@users.noreply.github.com>
2023-02-16 21:48:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+var linguagem = "Javascript"
+console.log("Aprendendo" + linguagem)
+//linguagem pode ser redeclarada 
+var linguagem = "Java"
+console.log("Aprendendo" +linguagem)
+
+//Escopo não restrito ao bloco 
+var idade = 18
+console.log("Oi, "+nome)
+    if(idade>=18){
+        var nome = "João"
+        console.log("Parabéns, "+nome + "Você pode dirigir.")
+    }
+    console.log("Até mais, " +nome + ".")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //declarando constantes 
+// const nome = "José"
+// const idade = 27
+
+// /*Comentário de 
+// multiplas linhas*/
+// const sexo = 'Masculino'
+// console.log(nome, idade, sexo)
+
+// //let: Declara variável 
+// let a = 2
+// let b = 3
+
+// console.log(a, b)
+// a = a + b 
+// console.log(a)
+
+// a = "SD & MOBILE"
+// console.log(a)
+
+// a = true 
+// console.log(a)
+
+// var unidadeCurricular = "DesWeb"
+// var deMaior = false
+// console.log(unidadeCurricular, deMaior)
